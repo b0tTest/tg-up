030b9e2236403fa01d01bd15c10fa723a1c45be6
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-25 17:23:42 +0530

    Added close button

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -283,16 +283,9 @@ async def echo(bot, update):
             im.save(thumb_image_path.replace(".webp", ".jpg"), "jpeg")
         else:
             thumb_image_path = None
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
-            reply_markup=reply_markup,
-            parse_mode="html",
-            reply_to_message_id=update.message_id
-        )
+        message_text = Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
-        inline_keyboard = []
         cb_string_file = "{}={}={}".format(
             "file", "LFO", "NONE")
         cb_string_video = "{}={}={}".format(
@@ -307,11 +300,12 @@ async def echo(bot, update):
                 callback_data=(cb_string_file).encode("UTF-8")
             )
         ])
-        reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(""),
-            reply_markup=reply_markup,
-            parse_mode="html",
-            reply_to_message_id=update.message_id
-        )
+        message_text = Translation.FORMAT_SELECTION.format("")
+    inline_keyboard.append([InlineKeyboardButton("✖️ CLOSE ✖️",callback_data="close")])
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
+    await update.reply_text(
+        text=message_text,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        quote=True
+    )
